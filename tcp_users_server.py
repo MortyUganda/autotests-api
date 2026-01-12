9c5a5eaca9ce676bf5a24469b971d09c9fbc91ca
MortyUganda <kurganskiy.1990@mail.ru>
2026-01-12 23:48:38 +0300

    Знакомство с TCP/IP протоколами. Практика работы с TCP/IP протоколом

---
+++ tcp_users_server.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    db = []
+    server_adress = ('localhost', 12345)
+    server_socket.bind(server_adress)
+
+    server_socket.listen(10)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f'Пользователь с адресом: {client_address} подключился к серверу')
+
+        data = client_socket.recv(1024).decode()
+        print(f'Пользователь с адресом: {client_address} отправил сообщение: {data}')
+
+        
+        db.append(data)
+
+        client_socket.send('\n'.join(db).encode())
+        client_socket.close()
+
+if __name__=='__main__':
+    server()
